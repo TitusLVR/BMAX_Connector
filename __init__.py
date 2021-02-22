c4800f488f384cdaebb300563222f349c1cac8bf
TitusLVR <Titus.mailbox@gmail.com>
2021-02-22 13:28:36 +0100

    Change version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "BMAX Connector",
     "author": "Titus Lavrov / Email: Titus.mailbox@gmail.com",
-    "version": (0, 1, 2),
+    "version": (0, 1, 3),
     "blender": (2, 80, 0),
     "location": "View3D > Toolbar and View3D",
     "warning": "",
